961f4700ca3358b5826e3bf36442b32a58b0f4b9
Tebogo <maitsapot@gmail.com>
2026-04-10 00:13:03 +0200

    fix: clean pycache and update database config

--- app/database.py
+++ app/database.py
@@ -3,7 +3,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker,declarative_base
 
-DATABASE_URL = "postgresql://eyetea:Lerato123@socialspaces-odoo-db.postgres.database.azure.com:5432/socialspaces?sslmode=require"
+DATABASE_URL = "postgresql://eyetea:Lerato123@socialspaces-db.postgres.database.azure.com:5432/socialspaces?sslmode=require"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
