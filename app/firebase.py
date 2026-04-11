17dc7f37c7a38390c878367c32454ad1225e83d2
Tebogo <maitsapot@gmail.com>
2026-04-11 02:27:32 +0200

    fix: firebase.py updated

--- app/firebase.py
+++ app/firebase.py
@@ -1,8 +1,14 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, auth
 
-# 🔥 Load service account file directly
-cred = credentials.Certificate("app/firebase_key.json")
+firebase_json = os.getenv("FIREBASE_KEY")
+
+if not firebase_json:
+    raise Exception("FIREBASE_KEY not set")
+
+cred = credentials.Certificate(json.loads(firebase_json))
 
 if not firebase_admin._apps:
     firebase_admin.initialize_app(cred)
